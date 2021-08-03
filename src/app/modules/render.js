cb2ddec44f50b722d1cd768deecd0973e5aae847
miguelesco <miuelangel1999@gmail.com>
2021-08-03 16:14:38 -0400

    fix arrow class

--- src/app/modules/render.js
+++ src/app/modules/render.js
@@ -1,16 +1,16 @@
 import { scoreChildren, scoreList } from './utilities.js';
 
 class Render {
-  reloadHTML() {
+  reloadHTML = () => {
     scoreList.innerHTML = '';
     scoreChildren.forEach((score, i) => {
-      this.count = i + 1;
-      this.bg = this.count % 2 ? 'white-bg' : 'gray-bg';
-      this.scoreElement = `
-            <li class="${this.bg}" >
+      const count = i + 1;
+      const bg = count % 2 ? 'white-bg' : 'gray-bg';
+      const scoreElement = `
+            <li class="${bg}" >
               ${score.textContent}
             </li>`;
-      scoreList.innerHTML += this.scoreElement;
+      scoreList.innerHTML += scoreElement;
     });
   }
 }
